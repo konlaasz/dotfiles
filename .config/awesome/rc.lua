90293dd1ef0ef1b08f0383700cd39b6972dea2f0
László Koncz <konlaasz@gmail.com>
2022-07-11 14:23:55 +0200

    Improve help tags

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -338,8 +338,8 @@ globalkeys = awful.util.table.join(
               {description = "show the menubar", group = "launcher"}),
 
     -- Backlight control
-    awful.key({ }, "XF86MonBrightnessUp", function () brightness_widget:inc() end, {description = "increase brightness", group = "screen"}),
-    awful.key({ }, "XF86MonBrightnessDown", function () brightness_widget:dec() end, {description = "decrease brightness", group = "screen"}),
+    awful.key({ }, "XF86MonBrightnessUp", function () brightness_widget:inc() end, {description = "increase screen brightness", group = "screen"}),
+    awful.key({ }, "XF86MonBrightnessDown", function () brightness_widget:dec() end, {description = "decrease screen brightness", group = "screen"}),
 
     --awful.key({ }, "XF86MonBrightnessDown", function () awful.spawn("light -U 5") end, {description = "increase screen brightness", group = "screen"}),
     --awful.key({ }, "XF86MonBrightnessUp", function () awful.spawn("light -A 5") end, {description = "decrease screen brightness", group = "screen"})
