b528f602f67785b0b740e2726e3ea9e153ee3962
László Koncz <konlaasz@gmail.com>
2022-08-25 01:18:05 +0200

    Add autorun script
    It would be nicer to set the compose key from xinitrc,
    but for some reason the option just gets ignored.

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -502,3 +502,7 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+-- {{{ Autorun
+awful.spawn.with_shell("~/.config/awesome/autorun.sh")
+-- }}}
