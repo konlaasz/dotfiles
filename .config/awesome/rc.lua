5437a4477512d2cfc73aa80a0bb7c0e763964c02
László Koncz <konlaasz@gmail.com>
2022-08-26 03:48:25 +0200

    Remove mouse bindings

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -209,13 +209,6 @@ awful.screen.connect_for_each_screen(function(s)
 end)
 -- }}}
 
--- {{{ Mouse bindings
-root.buttons(awful.util.table.join(
-    awful.button({ }, 4, awful.tag.viewnext),
-    awful.button({ }, 5, awful.tag.viewprev)
-))
--- }}}
-
 -- {{{ Key bindings
 globalkeys = awful.util.table.join(
     awful.key({ modkey,           }, "s",      hotkeys_popup.show_help,
