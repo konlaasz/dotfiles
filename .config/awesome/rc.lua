566b93143f090abe404bad7c47e13236d217c08f
László Koncz <konlaasz@gmail.com>
2022-09-05 22:30:35 +0200

    Add shortcuts for moving focus by id

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -257,6 +257,18 @@ globalkeys = gears.table.join(
         end,
         {description = "focus client to the right", group = "client"}
         ),
+    awful.key({ modkey,           }, "Down",
+        function ()
+            awful.client.focus.byidx( 1)
+        end,
+        {description = "focus next by index", group = "client"}
+        ),
+    awful.key({ modkey,           }, "Up",
+        function ()
+            awful.client.focus.byidx(-1)
+        end,
+        {description = "focus previous by index", group = "client"}
+        ),
     awful.key({ modkey,           }, "u", awful.client.urgent.jumpto,
               {description = "jump to urgent client", group = "client"}),
     awful.key({ modkey,           }, "Tab",
