b4979e27179269445126ef7dececefaabecb52ea
László Koncz <konlaasz@gmail.com>
2025-02-01 19:39:56 +0100

    P stands for (s)pawn

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -110,6 +110,7 @@ keys = [
     Key([mod, "control"], "r", lazy.restart(), desc="Restart Qtile"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
     Key([mod], "r", lazy.spawncmd(),
+    Key([mod], "p", lazy.spawncmd(),
         desc="Spawn a command using a prompt widget"),
     Key([mod], 's', lazy.spawn('slock')),
 
